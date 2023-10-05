e48444d23d936f1973f57dfdf6a617c20b37cb60
Brandon <brandontran11@live.com>
2023-10-04 20:13:59 -0400

    Update main.js

--- shipping-application/src/main/resources/static/main.js
+++ shipping-application/src/main/resources/static/main.js
@@ -8,11 +8,9 @@ function registerAcc() {
     accUsername = document.getElementById("loginUsername").value;
     accPassword = document.getElementById("loginPassword").value;
     accEmail = document.getElementById("email").value;
-    console.log(accUsername, accPassword, accEmail)
 }
 
 function login() {
     loginUsername = document.getElementById("username").value;
     loginPassword = document.getElementById("password").value;
-    console.log(loginUsername, loginPassword);
 }
