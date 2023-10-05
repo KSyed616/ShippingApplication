2413ceed87438579fc0e108188ac8a3bd4031072
Mrmoi356 <81328537+Mrmoi356@users.noreply.github.com>
2023-10-05 15:48:52 -0400

    Pushing table creation changes from Repository and Entity

--- shipping-application/src/main/java/com/cps714/objects/orders/Order.java
+++ shipping-application/src/main/java/com/cps714/objects/orders/Order.java
@@ -1,4 +1,33 @@
 package com.cps714.objects.orders;
 
+import com.cps714.objects.products.Product;
+import com.cps714.objects.users.Customer;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+import jakarta.persistence.*;
+
+@Data
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @NotNull
+    @Column(name = "order_number")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer orderNumber;
+
+    @NotNull
+    @Column(name = "order_date")
+    private String orderDate;
+
+    /*@ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;*/
+
 }
--- shipping-application/src/main/java/com/cps714/objects/users/Customer.java
+++ shipping-application/src/main/java/com/cps714/objects/users/Customer.java
@@ -1,4 +1,15 @@
 package com.cps714.objects.users;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
 }
